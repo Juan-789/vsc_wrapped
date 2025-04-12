a6fa5d28221e413c4ae5b9ebcd89603992270b2c
juan <juanmarulandadelosri@cmail.carleton.ca>
2025-04-12 00:37:04 -0400

    new command that saves to the globalStarage that every user has, the idea is that there the user's data will all be stored

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,25 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+async function saveDiagnosticsStat(context: vscode.ExtensionContext, newEntry: any) {
+	const fileUri = vscode.Uri.joinPath(context.globalStorageUri, 'codestats.json');
+
+	await vscode.workspace.fs.createDirectory(context.globalStorageUri);
+	let existing: any[] = [];
+
+	try {
+		const data = await vscode.workspace.fs.readFile(fileUri);
+		existing = JSON.parse(data.toString());
+	} catch (e) {
+		console.log('No existing file foung. Creating new one.');
+	}
+	
+	existing.push(newEntry);
+
+	await vscode.workspace.fs.writeFile(fileUri, Buffer.from(JSON.stringify(existing, null, 2)));
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -21,20 +40,23 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable);
 
-
-	const currentFile = vscode.window.activeTextEditor;
-	console.log(currentFile?.document.fileName);
-	if (currentFile) {
-		// const uri = currentFile.document.uri;
-		const diagnostics = vscode.languages.getDiagnostics();
-		diagnostics.forEach(([uri, diagnosticArray]) => {
-			diagnosticArray.forEach(diagnostic => {
-				vscode.window.showInformationMessage(diagnostic.message);
+	const writingToFile = vscode.commands.registerCommand('vscwrapped.saveStats', () =>{
+		const currentFile = vscode.window.activeTextEditor;
+		console.log(currentFile?.document.fileName);
+		if (currentFile) {
+			// const uri = currentFile.document.uri;
+			const diagnostics = vscode.languages.getDiagnostics();
+			diagnostics.forEach(([uri, diagnosticArray]) => {
+				diagnosticArray.forEach(diagnostic => {
+					vscode.window.showInformationMessage(diagnostic.message);
+					saveDiagnosticsStat(context, diagnostic.message);
+				});
+				// const dis2 = vscode.commands.registerCommand('vscwrapped.helloWorld', () => {
+				// 	vscode.window.showInformationMessage(diagnostic.message);
 			});
-			// const dis2 = vscode.commands.registerCommand('vscwrapped.helloWorld', () => {
-			// 	vscode.window.showInformationMessage(diagnostic.message);
-		});
-	}
+		}
+	});
+	
 }
 
 // This method is called when your extension is deactivated
